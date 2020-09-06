76f550f73b6b5aae5b76c234e5d7e1ba24812af9
Paige Miles <paige@lucas-miles.com>
2020-09-06 23:04:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,6 @@ def join_nested_strings(array_of_arrays)
     element_index = 0
     while element_index < array_of_arrays[row_index].count do
       if array_of_arrays[row_index][element_index].is_a? String
-        #Integer(array_of_arrays[row_index][element_index], exception: false) == nil
         collected_strings.push(array_of_arrays[row_index][element_index])
       end 
     element_index += 1
